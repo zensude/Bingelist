c10d7275d222b57273d1f85dc9a8b16011d070b7
Zeynep <zsena857@gmail.com>
2026-03-19 23:59:41 +0300

    Add search input and state handling in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Home() {
+const Home = () => {
+    const [search, setSearch] = useState("");
     return (
         <div>
-            <h1>Welcome to BingeList 🎬</h1>
+            <h1>Welcome to BingeList </h1>
+            <input
+                type="text"
+                placeholder="Search movies or series"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <p>Searching for:{search} </p>
         </div>
     )
 }
 
-export default Home
+export default Home;
