83937f3cb37d2730c29c188b943d4c78cd096a47
Zeynep <zsena857@gmail.com>
2026-04-26 00:34:05 +0300

    feat: improve favorite button UI with dynamic state

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function MovieCard({ movie, toggleFavorite, favorites }) {
     const isFav = favorites.some((m) => m.imdbID === movie.imdbID);
+
     return (
         <div style={{
             width: "200px",
@@ -16,29 +17,37 @@ function MovieCard({ movie, toggleFavorite, favorites }) {
                 src={movie.Poster !== "N/A" ? movie.Poster : "/no-image.png"}
                 onError={(e) => {
                     e.target.src = "/no-image.png";
-
                 }}
                 width={200}
                 height={300}
             />
+
             <div style={{ padding: "10px" }}>
                 <h3 style={{ fontSize: "16px", margin: "5px 0" }}>
                     {movie.Title}
                 </h3>
+
                 <p style={{ color: "gray", fontSize: "14px" }}>
                     {movie.Year}
-                    <br></br>
-
-                    <button style={{ backgroundColor: "gray" }} onClick={() => toggleFavorite(movie)}>
-                        {favorites.find((m) => m.imdbID === movie.imdbID)
-                            ? "❤️"
-                            : "🤍"}
-                    </button>
                 </p>
+
+                <button
+                    onClick={() => toggleFavorite(movie)}
+                    style={{
+                        backgroundColor: isFav ? "red" : "gray",
+                        color: "white",
+                        border: "none",
+                        padding: "6px 10px",
+                        borderRadius: "5px",
+                        cursor: "pointer"
+                    }}
+                >
+                    {isFav ? "❤️ Favorited" : "🤍 Add"}
+                </button>
             </div>
 
         </div>
     )
 }
 
-export default MovieCard
+export default MovieCard;
