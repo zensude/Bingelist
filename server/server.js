8608e36deb516b3523d7f4954347ea1d8be17115
Zeynep <zsena857@gmail.com>
2026-05-02 23:59:20 +0300

    Setup backend with Express server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Backend çalışıyor " });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
