5a2417b211bc5ddd4ecd973e08c33b39e0ba0951
Zeynep <zsena857@gmail.com>
2026-05-04 23:50:55 +0300

    Debug backend connection issue

--- server/server.js
+++ server/server.js
@@ -6,10 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
-    res.json({ message: "Backend çalışıyor " });
+    console.log("REQUEST RECEIVED");
+    res.json({ message: "Backend is running " });
 });
 
+// Start server
 app.listen(5000, () => {
-    console.log("Server running on port 5000");
+    console.log("Server is running on port 5000 ");
 });
