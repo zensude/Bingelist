8a451097972bb3b53c4960d55d872348323b24d2
Zeynep <zsena857@gmail.com>
2026-05-14 23:56:13 +0300

    Fix backend port conflict and restore server connection

--- server/server.js
+++ server/server.js
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-    console.log("Server is running on port 5000 ");
+app.listen(5001, () => {
+    console.log("Server is running on port 5001 ");
 });
